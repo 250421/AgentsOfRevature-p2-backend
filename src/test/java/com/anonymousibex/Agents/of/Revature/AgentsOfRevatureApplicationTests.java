b113626ef40db4e0dc558f9d6a3763b40cb8a611
Josh McCormack <joshgmc@gmail.com>
2025-05-07 17:10:52 -0400

    initialize

---
+++ src/test/java/com/anonymousibex/Agents/of/Revature/AgentsOfRevatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anonymousibex.Agents.of.Revature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentsOfRevatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
